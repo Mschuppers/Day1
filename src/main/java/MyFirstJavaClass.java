6474a74014a81548b1ac548de5f7b4414017e52f
Mschuppers <90316081+Mschuppers@users.noreply.github.com>
2021-09-08 11:58:34 +0200

    Update MyFirstJavaClass.java
    Attempt to make a pull

--- src/main/java/MyFirstJavaClass.java
+++ src/main/java/MyFirstJavaClass.java
@@ -3,5 +3,6 @@ public class MyFirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Commit number 2");
+        System.out.println("Commit number 3");
     }
 }
